9276caf7fe051f1407c781f8d265b0f3a078967a
deanna <42.dpiedra@gmail.com>
2022-04-04 17:09:27 +0200

    added function for creating ID

--- components/TodoApp.jsx
+++ components/TodoApp.jsx
@@ -4,20 +4,24 @@ import DisplayTodo from './DisplayTodo';
 
 export default function TodoApp() {
     const [items, setItems] = useState([
-      {id: 1, task: "buy milk", completed: false},
-      {id: 2, task: "buy eggs", completed: false}
+      {id: 0, task: "buy milk", completed: false},
+      {id: 1, task: "buy eggs", completed: false}
     ]);
   
     const addTodo = (newTodoText) => {
+        let newid = increaseId();
         if (newTodoText)
-            setItems([...items, {id: newTodoText, task: newTodoText, completed: false }]);
+            setItems([...items, {id: newid, task: newTodoText, completed: false }]);
     };
+
+    const increaseId = () => items.length;
   
     return (
         <div>
             <h1>What&apos;s there Todo?</h1>
             <TodoAdd addTodo={addTodo}/>
             <DisplayTodo todos={items}/>
+            {console.log(items)}
         </div>
     )
   }
