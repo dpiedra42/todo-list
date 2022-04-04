fabbcd87a729d8e31291a4addb2a6063f0144d48
deanna <42.dpiedra@gmail.com>
2022-04-04 15:41:38 +0200

    created component

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,10 @@
 import Image from 'next/image';
-import { useState } from 'react';
+import TodoApp from '../components/TodoApp';
 
 export default function Home() {
-  const [todoItem, setTodoItem] = useState('');
-  const [items, setItems] = useState([
-    "buy milk",
-    "buy eggs"
-  ]);
-
   return (
     <div>
-       <h1>What&apos;s there Todo?</h1>
-       <div>
-          <input type='text' value={todoItem} onChange={(e) => setTodoItem(e.target.value)}/>
-          <button type='button'>Add</button>
-        </div>
-        <ul>
-          {items.map((item) => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
+       <TodoApp/>
     </div>
   )
 }
