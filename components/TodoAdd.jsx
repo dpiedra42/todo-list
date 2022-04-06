b51ad172e8cce5982a5f9e90f1fcf70ab78e5bb0
deanna <42.dpiedra@gmail.com>
2022-04-06 15:48:43 +0200

    styled AddTodo

--- components/TodoAdd.jsx
+++ components/TodoAdd.jsx
@@ -1,16 +1,41 @@
 import useInputState from '../hooks/useInput';
+import styled from '@emotion/styled';
 
 export default function TodoAdd({addTodo}) {
     const [val, handleChange, reset] = useInputState('');
     return(
         <div>
-            <input type='text' placeholder="Add A Todo" value={val} onChange={(e) => handleChange(e)}/>
-            <button type='button' onClick={() => {
+            <AddTodo type='text' placeholder="Add A Todo" value={val} onChange={(e) => handleChange(e)}/>
+            <SubmitButton type='button' onClick={() => {
                 addTodo(val);
                 reset();
             }}>
                 Add
-            </button>
+            </SubmitButton>
         </div>
     )
 }
+
+const AddTodo = styled.input`
+    width: 20vw;
+    height: 3vh;
+    margin-right: 0.5vw;
+    padding-left: 1vw;
+    border: solid 0.2vw lightblue;
+    border-radius: 1vw;
+    font-size: 1vw;
+    font-family: 'Quicksand', sans-serif;
+`
+const SubmitButton = styled.button`
+    width: 3vw;
+    font-size: 1vw;
+    height: 3vh;
+    background-color: lightblue;
+    border: none;
+    border-radius: 1vw;
+    font-family: 'Quicksand', sans-serif;
+
+    :hover {
+        color: white;
+    }
+`
